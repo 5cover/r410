9c17ab4c91354e43eb71b70669d101a21f421bdf
Scover <bardini.raphael@gmail.com>
2025-03-04 13:17:26 +0100

    env files

--- src/sql/reset.bash
+++ src/sql/reset.bash
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
+# $ set -a && . .env.source
+
 set -eu
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+# shellcheck disable=SC2087
 
-psql -v ON_ERROR_STOP=on -d postgres -h "$DB_HOST" -wU "$DB_USER" \
-    -c "drop database if exists \"$DB_NAME\"" \
-    -c "create database \"$DB_NAME\"" \
-    -c "\c \"$DB_NAME\"" \
+scp . "debian@413.ventsdouest.dev:/tmp/sql"
+
+ssh debian@413.ventsdouest.dev sudo docker exec -iw / postgresdb psql \
+    -v ON_ERROR_STOP=on "postgres://$PGUSER:$PGPASS@$PGHOST:$PGPORT/postgres" \
+    -c "\"drop database if exists $PGDATABASE\"" \
+    -c "\"create database $PGDATABASE\"" \
+    -c "\"\c $PGDATABASE\"" \
     -f schema.sql \
     -f populate.sql
