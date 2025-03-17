4bd0c60c45a0e3746d53b29f5413a4b1d7c71edb
Scover <bardini.raphael@gmail.com>
2025-03-17 08:52:45 +0100

    basic map and geocoding

--- src/html/js/index.mjs
+++ src/html/js/index.mjs
@@ -1,1 +1,49 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+/** @import L from 'leaflet'; */
+
+const map = L.map('map').setView([46, 2], 4);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+/**
+ * @var {Map<string, string>} people
+ */
+const people = new Map();
+people.set("Laurent d'Orazio", 'Lannion, FR');
+
+const fallback_coords = [48.7446, -3.4592];
+
+/**
+ * @var {Map<string, [number, number]>}
+ */
+const coordinates = new Map();
+//coordinates.set("Lannion, FR", [48.7446, -3.4592]);
+
+for (const [name, city_country] of people) {
+    let coords = coordinates.get(city_country);
+    if (!coords) {
+        coords = await get_coordinates(...city_country.split(', '));
+        coordinates.set(city_country, coords);
+    }
+    // if null fetch api
+    const marker = L.marker(coords ?? fallback_coords).addTo(map);
+    marker.bindPopup(`${name}<br>${city_country}`);
+}
+
+/**
+ * @param {string} city
+ * @param {string} country
+ */
+async function get_coordinates(city, country) {
+    const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${city},${country}&format=json&limit=1`);
+    const data = await response.json();
+    if (data.length > 0) {
+        const location = data[0];
+        return [location.lat, location.lon];
+    } else {
+        return null;
+    }
+}
